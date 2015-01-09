bdf7fc380d4ace68220738b0d91a98d5bf4e3cb8
Mikhail Pilin <mikhail@pilin.name>
2015-01-09 11:38:48 +0100

    Add grace days parameters

--- HgCloseOldBranches.ps1
+++ HgCloseOldBranches.ps1
@@ -1,5 +1,5 @@
 # Close old branches for Mercurial
-# Copyright (C) 2014  Mikhail Pilin
+# Copyright (C) 2014-2015  Mikhail Pilin
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -15,41 +15,43 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 param(
-	[parameter(Mandatory=$false)]
-	[alias("p")]
-	[switch]
-	$performChanges = $false,
+  [parameter(Mandatory=$false)]
+  [alias("p")]
+  [switch]$performChanges = $false,
 
-	[parameter(Mandatory=$false)]
-	[alias("i", "ignore")]
-	[System.String[]]
-	$ignoreBranches = @()
+  [parameter(Mandatory=$false)]
+  [alias("i")]
+  [string[]]$ignoreBranches = @(),
+
+  [parameter(Mandatory=$false)]
+  [alias("g")]
+  [ValidateRange(1, 65536)]
+  [int]$graceDays = 60
 )
 
-if ($PSVersionTable.PSVersion.Major -lt 3) {
-  throw "PS Version $($PSVersionTable.PSVersion) is below 3.0."
-}
+if ($PSVersionTable.PSVersion.Major -lt 3) { throw "PS Version $($PSVersionTable.PSVersion) is below 3.0." }
 
 Set-StrictMode -Version Latest
 $ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop
 $script:VerbosePreference = "Continue"
-
 $encoding = "cp866"
-[System.TimeSpan]$alive = [System.TimeSpan]::FromDays(60)
+
+Write-Host "Close branches older than: " -nonewline
+Write-Host -foregroundcolor darkgray $graceDays "days"
+
+[System.TimeSpan]$alive = [System.TimeSpan]::FromDays($graceDays)
 [System.DateTime]$now = [System.DateTime]::Now
 
-function GetHgRoot
-{
-    Param([System.String]$path)
-
-    if (Test-Path -Path "$path\.hg" -PathType Container) {
-        return $path
-    } else {
-        $parent = Split-Path -Path "$path" -Parent
-        if ($parent.Length -gt 0) {
-          GetHgRoot $parent
-        }
+function GetHgRoot {
+  Param([string]$path)
+  if (Test-Path -Path "$path\.hg" -PathType Container) {
+    return $path
+  } else {
+    $parent = Split-Path -Path "$path" -Parent
+    if ($parent.Length -gt 0) {
+      GetHgRoot $parent
     }
+  }
 }
 
 $root = GetHgRoot (Get-Item -Path .).FullName
@@ -59,30 +61,29 @@ Write-Host "Repository root: " -nonewline
 Write-Host -foregroundcolor darkgray $root
 
 [System.Object]$current = & hg parent --encoding $encoding -T "{node} {branch}" | ForEach-Object {
-    $result = $_ | Select-Object -Property node, name
-    $parts = $_.Split(' ', 2)
-    
-    $result.node = $parts[0]
-    $result.name = $parts[1]
-    $result
-  } | Select-Object
+  $result = $_ | Select-Object -Property node, name
+  $parts = $_.Split(' ', 2)
+
+  $result.node = $parts[0]
+  $result.name = $parts[1]
+  $result
+} | Select-Object
 
 Write-Host "Current branch: " -nonewline
 Write-Host -foregroundcolor gray $current.name
 
 [System.Object[]]$branches = @(& hg head --encoding $encoding -T "{date(date,'%Y%m%d%H%M%S')} {node} {branch}\n" | ForEach-Object {
-    $result = $_ | Select-Object -Property date, node, name
-    $parts = $_.Split(' ', 3)
+  $result = $_ | Select-Object -Property date, node, name
+  $parts = $_.Split(' ', 3)
 
-    [System.DateTime]$result.date = [System.DateTime]::ParseExact($parts[0], "yyyyMMddHHmmss", [System.Globalization.CultureInfo]::InvariantCulture)
-    $result.node = $parts[1]
-    $result.name = $parts[2]
-    $result
-  } | Sort-Object -Property date)
+  [System.DateTime]$result.date = [System.DateTime]::ParseExact($parts[0], "yyyyMMddHHmmss", [System.Globalization.CultureInfo]::InvariantCulture)
+  $result.node = $parts[1]
+  $result.name = $parts[2]
+  $result
+} | Sort-Object -Property date)
 
-function EscapeBranchName
-{
-  Param([System.String]$name)
+function EscapeBranchName {
+  Param([string]$name)
   $name.Replace('"', '\"')
 }
 
@@ -126,20 +127,20 @@ if ($branches.Length -eq 0) { Write-Host "No old branches were detected" } else 
   Write-Host "Closing $($branches.Length) old branches:"
 
   $branches | ForEach-Object {
-      Write-Host "[$([System.String]::Format("{0:%d}", $now - $_.date)) days] " -nonewline
-      Write-Host -foregroundcolor gray $_.name
+    Write-Host "[$([string]::Format("{0:%d}", $now - $_.date)) days] " -nonewline
+    Write-Host -foregroundcolor gray $_.name
 
-      if ($performChanges) {
-        & hg debugsetparent $_.node | Out-Null
-        if ($LastExitCode -ne 0) { Write-Warning "Failed to set node (exit code $LastExitCode)." } else {
-          & hg branch $(EscapeBranchName $_.name) | Out-Null
-          if ($LastExitCode -ne 0) { Write-Warning "Failed to set branch (exit code $LastExitCode)." } else {
-            & hg commit --close-branch -X * -m $("The branch was not used for {0:%d} days and closed automatically." -f ($now - $_.date)) | Out-Null
-            if ($LastExitCode -ne 0) { Write-Warning "Failed to commit (exit code $LastExitCode)." }
-          }
+    if ($performChanges) {
+      & hg debugsetparent $_.node | Out-Null
+      if ($LastExitCode -ne 0) { Write-Warning "Failed to set node (exit code $LastExitCode)." } else {
+        & hg branch $(EscapeBranchName $_.name) | Out-Null
+        if ($LastExitCode -ne 0) { Write-Warning "Failed to set branch (exit code $LastExitCode)." } else {
+          & hg commit --close-branch -X * -m $("The branch was not used for {0:%d} days and closed automatically." -f ($now - $_.date)) | Out-Null
+          if ($LastExitCode -ne 0) { Write-Warning "Failed to commit (exit code $LastExitCode)." }
         }
       }
     }
+  }
 
   if ($hasHgSubState) {
     Write-Host "Restore: " -nonewline
